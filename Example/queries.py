00c14a4d2f23060fb1b04a86374a9d0124d70d0b
mohammadsuhail98 <msuhayl@earthlinktele.com>
2023-05-01 20:57:16 +0200

    Created queries file and created query to fetch the airlines with arrival delay.

---
+++ Example/queries.py
@@ -0,0 +1,41 @@
+import json
+import pandas as pd
+from pymongo import MongoClient
+from pprint import pprint
+
+
+mongoCollection = None
+
+
+def get_puntual_airlines():
+    mongoCollection = db['flights']
+    pipeline = [
+    { "$match": { "CANCELLED": 0, "ARRIVAL_DELAY": { "$lte": 0 } } },
+    { "$group": { "_id": "$AIRLINE", "avg_arrival_delay": { "$avg": "$ARRIVAL_DELAY" } } },
+    { "$lookup": { "from": "airlines", "localField": "_id", "foreignField": "IATA_CODE", "as": "airline_info" } },
+    { "$project": { "airline_name": { "$arrayElemAt": [ "$airline_info.AIRLINE", 0 ] }, "avg_arrival_delay": 1 } },
+    { "$sort": { "avg_arrival_delay": -1 } }
+    ]   
+
+    results = mongoCollection.aggregate(pipeline)
+    
+    json_results = []
+    for result in results:
+        json_results.append({
+            "airline_name": result['airline_name'],
+            "_id": result['_id'],
+            "avg_arrival_delay": round(result['avg_arrival_delay'], 2)
+        })
+    
+    return json.dumps(json_results, indent=4)
+
+    
+if __name__ == '__main__':
+    try:
+        client = MongoClient('localhost', 27017)
+        db = client['data_visualization_project']
+
+        json = get_puntual_airlines()
+        print(json)
+    except Exception as e:
+        print(e)
