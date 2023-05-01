64edcecf6a75aa761199b83ca56d651c69ba7103
mohammadsuhail98 <msuhayl@earthlinktele.com>
2023-05-01 22:50:56 +0200

    Modified the query and made it a class and added a query to fetch the airlines with no delays

--- Example/queries.py
+++ Example/queries.py
@@ -4,30 +4,59 @@ from pymongo import MongoClient
 from pprint import pprint
 
 
-mongoCollection = None
-
-
-def get_airlines_with_delays():
-    mongoCollection = db['flights']
-    pipeline = [
-    { "$match": { "CANCELLED": 0, "ARRIVAL_DELAY": { "$lte": 0 } } },
-    { "$group": { "_id": "$AIRLINE", "avg_arrival_delay": { "$avg": "$ARRIVAL_DELAY" } } },
-    { "$lookup": { "from": "airlines", "localField": "_id", "foreignField": "IATA_CODE", "as": "airline_info" } },
-    { "$project": { "airline_name": { "$arrayElemAt": [ "$airline_info.AIRLINE", 0 ] }, "avg_arrival_delay": 1 } },
-    { "$sort": { "avg_arrival_delay": -1 } }
-    ]   
-
-    results = mongoCollection.aggregate(pipeline)
+class Queries:
+    mongoCollection = None
     
-    json_results = []
-    for result in results:
-        json_results.append({
-            "airline_name": result['airline_name'],
-            "_id": result['_id'],
-            "avg_arrival_delay": round(result['avg_arrival_delay'], 2)
-        })
+    def __init__(self, db):
+        self.db = db
     
-    return json.dumps(json_results, indent=4)
+    # =================================
+    # Gets the airlines with delays and sorts them from the least delay to the most delays
+    # =================================
+    def get_airlines_with_delays(self):
+        mongoCollection = self.db['flights']
+        pipeline = [
+        { "$match": { "CANCELLED": 0, "ARRIVAL_DELAY": { "$lte": 0 } } },
+        { "$group": { "_id": "$AIRLINE", "avg_arrival_delay": { "$avg": "$ARRIVAL_DELAY" } } },
+        { "$lookup": { "from": "airlines", "localField": "_id", "foreignField": "IATA_CODE", "as": "airline_info" } },
+        { "$project": { "airline_name": { "$arrayElemAt": [ "$airline_info.AIRLINE", 0 ] }, "avg_arrival_delay": 1 } },
+        { "$sort": { "avg_arrival_delay": -1 } }
+        ]   
+
+        results = mongoCollection.aggregate(pipeline)
+        
+        json_results = []
+        for result in results:
+            json_results.append({
+                "airline_name": result['airline_name'],
+                "_id": result['_id'],
+                "avg_arrival_delay": round(result['avg_arrival_delay'], 2)
+            })
+        
+        return json.dumps(json_results, indent=4)
+
+    # =================================
+    # Gets the airlines with no delays
+    # =================================
+    def get_airlines_with_no_delays(self):
+        mongoCollection = self.db['flights']
+        pipeline = [
+            {"$match": {"CANCELLED": 0, "ARRIVAL_DELAY": 0}},
+            {"$group": {"_id": "$AIRLINE"}},
+            {"$lookup": {"from": "airlines", "localField": "_id", "foreignField": "IATA_CODE", "as": "airline_info"}},
+            {"$project": {"airline_name": {"$arrayElemAt": ["$airline_info.AIRLINE", 0]}, "_id": 1}}
+        ]
+
+        results = mongoCollection.aggregate(pipeline)
+                
+        json_results = []
+        for result in results:
+            json_results.append({
+                "airline_name": result['airline_name'],
+                "_id": result['_id']
+            })
+                
+        return json.dumps(json_results, indent=4) 
 
     
 if __name__ == '__main__':
@@ -35,7 +64,10 @@ if __name__ == '__main__':
         client = MongoClient('localhost', 27017)
         db = client['data_visualization_project']
 
-        json = get_airlines_with_delays()
+        queries = Queries(db)
+
+        json = queries.get_airlines_with_no_delays()
         print(json)
+        
     except Exception as e:
         print(e)
